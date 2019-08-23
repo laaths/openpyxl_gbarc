fe8c08e08954c16b6e5e833a5ace084d0e279c4a
Leonardo <leonardo_soares@live.com>
2019-08-23 19:25:19 -0300

    first commit

---
+++ retiradas.py
@@ -0,0 +1,14 @@
+from retiradas_class import cadastrar, arquivos
+
+planilha = 'nova.xlsx' # Digita o nome da planilha sendo utilizada
+aba_plan = 'aba_sheet' # Nome da aba dentro do arquivo que vai utilizar
+
+cad = cadastrar(input("Nome: ").capitalize(), input("Nº Serie: "))
+read = arquivos()
+wb = read.rArq(planilha, aba_plan)
+aba = wb[aba_plan]
+ws = wb.active
+
+cad.cadastramento(aba, ws)
+
+wb.save(planilha)
