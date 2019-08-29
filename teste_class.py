eb3b39153cebcef023bf05bd02e405e099981ee7
Leonardo <leonardo_soares@live.com>
2019-08-29 14:59:50 -0300

    Inicio menu de opções

--- teste_class.py
+++ teste_class.py
@@ -1,65 +1,72 @@
 from openpyxl import load_workbook, Workbook
 
+# Class para carregamento do arquivo de leitura
 class loadArq:
     def __init__(self, planilha, aba_sheet):
         self.plan = planilha
         self.sheet = aba_sheet
         self.alfab = '0ABCDEFGHIJKLMNOPQRSTUVWXYZ'
 
+    # Carregar a planilha
     def loadPlan(self):
         return load_workbook(self.plan)
 
+    # Carregar a Aba da Planilha
     def loadSheet(self):
         return self.loadPlan()[self.sheet]
 
+    # Ativar edição da planilha
     def activePlan(self):
         return self.loadPlan().active
 
+    # Salvar planilha carregada
     def savePlan(self):
         return self.loadPlan().save(self.plan)
 
+    # Criar lista com itens da linha
     def listCols(self, proc, load):
         cols = []
         for cl in range(load.max_column ):
             for ln in range( load.max_row ):
-                position = load[self.alfab[cl + 1] + str( ln + 1 )].value
+                position = load.cell(row=ln+1, column=cl+1).value
                 if position == proc:
                     for cl2 in range(load.max_column ):
-                        cols.append(load[self.alfab[cl2+1]+str(ln+1)].value)
+                        cols.append(load.cell(row=ln+1, column=cl2+1).value)
                     return cols
                 else:
                     pass
         return False
 
+    # Criar lista com Colunas do painel
+    def listColsPanel(self, load):
+        cols = []
+        for cl in range(load.max_column):
+            cols.append(load.cell(row=1, column=cl+1).value)
+        return cols
+
+
+    # Criar lista com itens da coluna
     def listRows(self, proc, load):
         rows = []
         for ln in range(load.max_row):
             pass
 
-    def insertDados(self, aba, ws, dados):
-        x = 2
-        while True:
-            cel = str(x)
-            if aba['G' + cel].value == None:
-                for y in range(len(dados)):
-                    ws[self.alfab[y+1]+cel] = dados[y]
-                break
+    # Inserir dados na planilha
+    def insertDadosRow(self, dest_panel, orig_panel, ws, orig_dados):
+        dados = []
+        #for plan in range(ws.max_row):
+        for cl in range(len(dest_panel)):
+            if dest_panel[cl] in orig_panel:
+                #print('Existe na planilha')
+                dados.append(orig_dados[orig_panel.index(dest_panel[cl])])
+            elif dest_panel[cl] not in orig_panel:
+                dados.append(None)
             else:
-                x+=1
-        return ws
+                pass
+        return dados
 
+# Classe para salvar o arquivo / provavelmente não utilizado
 class saveArq:
     def __init__(self, planilha, aba_sheet):
         self.plan = planilha
         self.sheet = aba_sheet
-
-    def cadastramento(self, aba, ws):
-        x = 2
-        while True:
-            cel = str( x )
-            if aba['A' + cel].value == None:
-
-                break
-            else:
-                x += 1
-        return
--- teste_plan.py
+++ teste_plan.py
@@ -1,56 +1,66 @@
 from teste_class import loadArq, saveArq
 from teste_msg import msgs
 
-orig = ['planilhas/copia.xlsx', 'COMPLETA']
+orig = ['planilhas/copia.xlsx', 'Planilha1']
 dest = ['planilhas/lotericas_controle.xlsx', 'COMPLETA']
-proc = '18-017811-3'
+proc = 'PAE 0845598'
 
-
+# Procura e retorna lista com os dados
 def procPlan(planilha, aba, proc):
-    plan = loadArq(planilha, aba)
+    plan = loadArq( planilha, aba )
     ws = plan.loadSheet()
-    tt = plan.listCols(proc, ws)
+    tt = plan.listCols( proc, ws )
     return tt
 
+
 def insertPlan(orig_plan, orig_aba, dest_plan, dest_aba, proc):
-    orig_dados = procPlan(orig_plan, orig_aba, proc)
-    dest_dados = procPlan(dest_plan, dest_aba, proc)
+    orig_dados = procPlan( orig_plan, orig_aba, proc ) # Retorna dados procurado
+    dest_dados = procPlan( dest_plan, dest_aba, proc ) # Retorna dados procurado
     if orig_dados is False:
-        print("Não Encontrado")
+        print( "Não Encontrado" )
         return
     elif dest_dados != False:
-        print("Existente na planilha")
+        print( "Existente na planilha" )
         return
     else:
-        plan = loadArq(dest_plan, dest_aba)
-        wb = plan.loadPlan()
-        aba = wb[dest_aba]
-        ws = wb.active
-        plan.insertDados(aba, ws, orig_dados)
-        wb.save(dest_plan)
+        plandest = loadArq( dest_plan, dest_aba )
+        wbdest = plandest.loadPlan()
+        planorig = loadArq( orig_plan, orig_aba )
+        orig_panel = plandest.listColsPanel( planorig.loadSheet() ) # Retorna nomes da coluna do painel fixo
+        dest_panel = plandest.listColsPanel( plandest.loadSheet() ) # Retorna nomes da coluna do painel fixo
+        ws = wbdest.active
+        dad_ext = plandest.insertDadosRow( dest_panel, orig_panel, ws, orig_dados ) # Retorna lista do conteudo ordenado pela painel de destino
+        ws.append( dad_ext ) # Adiciona dados de uma lista na proxima linha vazia
+        wbdest.save( dest_plan ) # Salva o conteudo na planilha
         return
 
 
+def localizarDados():
+    pass
 
-insertPlan(orig[0], orig[1], dest[0], dest[1], proc)
+# print(procPlan(dest[0], dest[1], proc))
+# print(painel(orig[0], orig[1]))
 
+# Menu de opções
 def menuOpt():
     while True:
         msgs().getMenu()
-        opt = input(str("OPÇÃO DESEJADA: ").upper())
+        opt = input( str( "OPÇÃO DESEJADA: " ).upper() )
         if opt == 'Q':
             pass
-        elif opt == 'A': # COPIAR E COLAR LINHA DE UMA PLANILHA PRA OUTRA
+        elif opt == 'A':  # COPIAR E COLAR LINHA DE UMA PLANILHA PRA OUTRA
+            insertPlan( orig[0], orig[1], dest[0], dest[1], proc )
+        elif opt == 'B':  # PROCURAR
             pass
-        elif opt == 'B': # PROCURAR
-            pass
-        elif opt == 'C': # INSERIR LOOPBACK
+        elif opt == 'C':  # INSERIR LOOPBACK
             pass
         elif opt == 'D':
             pass
         elif opt == 'E':
-            pass
+            localizarDados()
         elif opt == 'F':
             pass
         else:
             msgs().getMsg1()
+
+menuOpt()
